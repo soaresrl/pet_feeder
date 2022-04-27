6ba30d56d0632604e4ec2d1cc12c55cff2a5f518
unknown <rodrigolucassoares@gmail.com>
2022-04-27 15:23:00 -0300

    set interval function

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,42 @@ app.use(express.json());
 
 const PORT = 3001;
 
+function scheduleExecution(scheduledHours: string[], callback: ()=>void) {
+    const maxInterval = 60 * 1000;
+
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+
+    const timeString = `${hours}:${minutes}:00`;
+
+    scheduledHours.forEach((time: string, index)=>{
+        if(timeString === time){
+            console.log('runned function');
+
+            const execProcess = spawn("python", ["server/src/servo.py"]);
+    
+            execProcess.stdout.on('spawn', ()=>{
+                console.log('spawn on spawn');
+            });
+            
+            execProcess.stdout.on('data', (data)=>{
+                console.log(`spawn stdout ${data}`);
+            });
+            
+            execProcess.stderr.on('data', (data)=>{
+                console.log(`spawn error ${data}`);
+            });
+        }
+    });
+
+    setTimeout(() => scheduleExecution(scheduledHours, callback), maxInterval);
+}
+
+const scheduledHours = ["08:00:00", "12:00:00", "20:00:00"];
+
+scheduleExecution(scheduledHours, ()=>{console.log('runned')});
+
 app.get('/feed', (req, res) => {
     console.log('called process');
 
@@ -24,7 +60,7 @@ app.get('/feed', (req, res) => {
         console.log(`spawn error ${data}`);
     });
 
-    res.status(201).json({msg: 'feeded your pet successfully'});
+    res.status(201).json({msg: 'feeded your pet manually successfully'});
 
 });
 
